26cd05cec20e9e9344e878a0104ba68bff7cd505
Rafał Mosakowski <mosakowski.r@gmail.com>
2022-07-30 13:36:46 +0200

    refactored index.js and added remove items from list function

--- src/index.js
+++ src/index.js
@@ -1,18 +1,145 @@
-const taskList = document.querySelector('.tasks');
+import { compareAsc, format } from 'date-fns'
 
-const todos = 
-JSON.parse(localStorage.getItem('todos')) || 
-[
-    {
-        category: 'Not categorized',
-        tasks: [],
+const addProjectBtn = document.querySelector('.add-project-btn')
+const inputProjectDiv = document.querySelector('#input-project-div')
+const submitProjectBtn = document.querySelector('.submit-project-btn')
+const cancelProjectBtn = document.querySelector('.cancel-project-btn')
+const projectNameInput = document.querySelector('.project-title')
+const removeProjectBtn = document.querySelector('.remove-project')
+
+const addTaskBtn = document.querySelector('.add-task-btn')
+const inputTaskDiv = document.querySelector('#input-task-div')
+const submitTaskBtn = document.querySelector('.submit-task-btn')
+const cancelTaskBtn = document.querySelector('.cancel-task-btn')
+const taskNameInput = document.querySelector('.task-title')
+const taskDateInput = document.querySelector('input[type="date"]')
+const taskListDiv = document.querySelector('.content')
+const taskList = document.querySelector('.tasks')
+
+const LOCAL_STORAGE_TODO_LIST = 'todos'
+const LOCAL_STORAGE_SELECTED_PROJECT = 'selectedProject'
+const LOCAL_STORAGE_SELECTED_TASK = 'selectedTask'
+
+let todoList = JSON.parse(localStorage.getItem(LOCAL_STORAGE_TODO_LIST)) || []
+let selectedProject = localStorage.getItem(LOCAL_STORAGE_SELECTED_PROJECT);
+let selectedTask = localStorage.getItem(LOCAL_STORAGE_SELECTED_TASK);
+
+addProjectBtn.addEventListener('click', (e) => {
+    e.target.classList.add('hide');
+    inputProjectDiv.classList.remove('hide')
+})
+
+
+
+submitProjectBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    if(projectNameInput.value === '' || projectNameInput.value == null) return
+    if(ifExist(projectNameInput.value, todoList) === true) return alert('This project already exist')
+    addProjectBtn.classList.remove('hide');
+    inputProjectDiv.classList.add('hide');
+    const list = createProjectList(projectNameInput.value)
+    projectNameInput.value = null;
+    todoList.push(list)
+    renderProject()
+    save()
+    renderTask()
+})
+
+submitTaskBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    console.log(taskDateInput.value)
+    if(taskNameInput.value === '' || taskNameInput.value == null) return
+    if(taskDateInput.value === '' || taskDateInput.value == null) {
+        taskDateInput.value = format(new Date(), 'yyyy-MM-dd')
     }
-];
+    addTaskBtn.classList.remove('hide');
+    inputTaskDiv.classList.add('hide');
+    const list = createTaskList(taskNameInput.value, taskDateInput.value)
+    taskNameInput.value = null;
+    taskDateInput.value = null;
+    const selectedProjectArr = todoList.find(task => task.category === selectedProject)
+    selectedProjectArr.tasks.push(list)
+    renderProject()
+    save()
+    renderTask()
+})
 
-let selectedProject = "Not categorized";
-console.log(selectedProject)
+const createProjectList = (name) => {
+    return { category: name, tasks: []  }
+}
 
-// check if project exist already in array
+const createTaskList = (name, date) => {
+    return { name: name, date: date }
+}
+
+const save = () => {
+    localStorage.setItem(LOCAL_STORAGE_TODO_LIST, JSON.stringify(todoList))
+    localStorage.setItem(LOCAL_STORAGE_SELECTED_PROJECT, selectedProject)
+    localStorage.getItem(LOCAL_STORAGE_SELECTED_TASK, selectedTask)
+}
+
+
+cancelProjectBtn.addEventListener('click', () => {
+    addProjectBtn.classList.remove('hide');
+    inputProjectDiv.classList.add('hide')
+})
+
+addTaskBtn.addEventListener('click', (e) => {
+    e.target.classList.add('hide');
+    inputTaskDiv.classList.remove('hide')
+})
+
+submitTaskBtn.addEventListener('click', (e) => {
+    if(taskNameInput.value === '') return
+    addTaskBtn.classList.remove('hide');
+    inputTaskDiv.classList.add('hide')
+})
+
+
+cancelTaskBtn.addEventListener('click', () => {
+    addTaskBtn.classList.remove('hide');
+    inputTaskDiv.classList.add('hide')
+})
+
+const projectList = document.querySelector('.projects-list')
+
+projectList.addEventListener('click', (e) => {
+    if(e.target.tagName.toLowerCase() === 'li') {
+        selectedProject = e.target.id;
+        renderProject()
+        save()
+        renderTask()
+    } else if(e.target.tagName.toLowerCase() === 'span') {
+        todoList = todoList.filter(item => item.category !== selectedProject)
+        selectedProject = null
+        save()
+        renderProject()
+        renderTask()
+    }
+    
+})
+
+taskList.addEventListener('click', (e) => {
+    if(e.target.tagName.toLowerCase() === 'p') {
+        selectedTask = e.target.id;
+        renderProject()
+        save()
+        renderTask()
+    } else if(e.target.tagName.toLowerCase() === 'span') {
+      todoList.forEach(arr => {
+        arr.tasks.forEach (ele => {
+            if(ele.name === selectedTask) {
+                arr.tasks.splice(arr.tasks.indexOf(ele), 1);
+            }
+        })
+    })
+        selectedTask = null
+        save()
+        renderProject()
+        renderTask()
+    }
+})
+
 const ifExist = (obj, list) => {
     for (let i = 0; i < list.length; i++) {
         if (list[i].category === obj) {
@@ -22,217 +149,56 @@ const ifExist = (obj, list) => {
     return false;
 }
 
-//add new project to todos array
-
-const findTask = (task) => {
-    todos.forEach(arr => {
-        arr.tasks.forEach (ele => {
-            if(ele === task) {
-                console.log(ele, arr.category)
-            }
-        })
-    })
-}
-findTask('banan')
-
-const newProject = (name) => {
-    const project = {
-        category: name,
-        tasks: []};
-        
-    if(ifExist(name, todos) !== true) {
-        todos.push(project);
-        localStorage.setItem('todos', JSON.stringify(todos));
-        renderProjects()
-    }
-    return todos
-}
-
-//add new task to project
-const addTask = (task, category = "Not categorized") => {
-    for (let i = 0; i < todos.length; i++) {
-        if (todos[i].category === category) {
-            let arr = todos[i].tasks;
-            let oldTask = arr.find(el => el === task)
-            if (oldTask === task) return;
-            todos[i].tasks.push(task);
-            localStorage.setItem('todos', JSON.stringify(todos));
-        } 
-    }
-    return todos
-}
-
-//remove project from todos
-const removeProject = (category) => {
-    for (let i = 0; i < todos.length; i++) {
-        if (todos[i].category === category) {
-            todos.splice(i, 1)
-            localStorage.setItem('todos', JSON.stringify(todos));
-        } 
-    }
-    return todos
-}
-
-//remove task form todo list
-const removeTask = (task, category = "Not Categorized") => {
-    todos.forEach(arr => {
-        arr.tasks.forEach (ele => {
-            if(ele === task) {
-                arr.tasks.splice(ele, 1);
-                localStorage.setItem('todos', JSON.stringify(todos))
-            }
-        })
-    })
-
-    return todos
-
-    // for (let i = 0; i < todos.length; i++) {
-    //     if (todos[i].category === category) {
-    //         for(let j = 0; j < todos[i].tasks.length; j++) {
-    //             if(todos[i].tasks[j] === task) {
-    //                 todos[i].tasks.splice(j, 1)
-    //                 localStorage.setItem('todos', JSON.stringify(todos));
-    //             }
-    //         }
-
-    //     } 
-    // }
-    // return todos
-}
-
-let arr = []
-
-const filteredArr = (cat) => {
-    arr = []
-    todos.filter((item) => {
-        if(item.category === cat) {
-            item.tasks.forEach(el => arr.push(el))
+const renderProject = () => {
+    projectList.innerHTML = ''
+    todoList.forEach(project => {
+        const projectItem = document.createElement('li');
+        projectItem.id = project.category
+        projectItem.classList.add('flex')
+        projectItem.textContent = project.category;
+        const span = document.createElement('span')
+        span.classList.add('remove-project');
+        span.textContent = '🗑';
+        if (project.category === selectedProject) {
+            projectItem.classList.add('selected')
         }
-    }) 
-    return arr
-}
-filteredArr(selectedProject)
+        projectItem.appendChild(span)
+        projectList.appendChild(projectItem)
 
-const renderTasks = (cat) => {
+    })
+}
+
+
+const projectTitle = document.querySelector('.projectTitle')
+
+const renderTask = () => {
+    if(selectedProject == 'null' || selectedProject == null) {
+        taskListDiv.classList.add('hide')
+    } else {
+        taskListDiv.classList.remove('hide')
+        projectTitle.textContent = selectedProject
+    }
+
     taskList.innerHTML = ''
-    displayedTitle.innerHTML = cat;
-    displayedTitle.id = cat;
-    console.log(arr)
-    arr.forEach(item => {
-        const taskDiv = document.createElement('div');
-        taskDiv.textContent = item;
-        taskList.appendChild(taskDiv)
-    })   
+    const selectedProjectArr = todoList.find(task => task.category === selectedProject)
+    selectedProjectArr.tasks.forEach(task => {
+        const taskItem = document.createElement('div');
+        taskItem.classList.add('flex')
+        const taskTitle = document.createElement('p')
+        taskTitle.id = task.name
+        taskTitle.textContent = `${task.name}, due Date: ${task.date}`;
+        const taskSpan = document.createElement('span')
+        taskSpan.classList.add('remove-task')
+        taskSpan.textContent = '🗑'
+        if (task.name === selectedTask) {
+            taskTitle.classList.add('selected')
+        }
+        taskItem.append(taskTitle, taskSpan)
+        taskList.appendChild(taskItem)
+
+    })
 }
 
-const projectListDiv = document.querySelector('.projects-list')
 
-const renderProjects = () => {
-    projectListDiv.innerHTML = ''
-    todos.forEach(element => {
-        const taskDiv = document.createElement('li');
-        taskDiv.className = 'projectItem';
-        taskDiv.id = element.category
-        taskDiv.textContent = element.category;
-        projectListDiv.appendChild(taskDiv)
-    })   
-}
-
-const displayedTitle = document.querySelector('.projectTitle')
-
-const renderProjectArr = () => {
-    const projectArr = document.querySelectorAll('.projectItem')
-    // const projectNameArr = []
-    // projectArr.forEach(el => {
-    //     projectNameArr.push(el.id)
-    // })
-
-    projectArr.forEach(el => el.addEventListener('click', (e) => {
-        taskList.innerHTML = ''
-        selectedProject = e.target.id
-        filteredArr(selectedProject)
-        displayedTitle.innerHTML = e.target.id;
-        displayedTitle.id = e.target.id;
-        taskList.id = e.target.id;
-        arr.forEach(item => {
-            const taskDiv = document.createElement('div');
-            taskDiv.textContent = item;
-            taskList.appendChild(taskDiv)
-
-        })}))
-}
-
-//show input to add task
-
-const addTaskBtn = document.querySelector('.add-task')
-const inputDiv = document.querySelector('#input-div')
-const cancelBtn = document.querySelector('.cancel-btn')
-const addBtn = document.querySelector('.addBtn')
-const taskNameInput = document.querySelector('.task-title')
-const formDiv = document.querySelector('.form-task')
-const projectTitle = document.querySelector('.tasks')
-
-
-addTaskBtn.addEventListener('click', (e) => {
-    e.target.classList.add('hide');
-    inputDiv.classList.remove('hide')
-})
-
-addBtn.addEventListener('click', (e) => {
-    e.preventDefault()
-    if(taskNameInput.value === '') return
-    addTaskBtn.classList.remove('hide');
-    inputDiv.classList.add('hide')
-    addTask(taskNameInput.value, taskList.id) // projectTitle.id dodac pozniej
-    renderTasks(selectedProject)
-    clearInputs()
-})
-
-cancelBtn.addEventListener('click', () => {
-    addTaskBtn.classList.remove('hide');
-    inputDiv.classList.add('hide')
-})
-
-const clearInputs = () => {
-    document.querySelector('form').reset()
-};
-
-//show input to add project
-
-const addProjectBtn = document.querySelector('.add-new')
-const inputProjectDiv = document.querySelector('#input-project')
-const cancelProjectBtn = document.querySelector('.cancel-project-btn')
-const submitProjectBtn = document.querySelector('.addProjectBtn')
-const projectNameInput = document.querySelector('.project-title')
-const formProjectDiv = document.querySelector('.form-project')
-
-addProjectBtn.addEventListener('click', (e) => {
-    e.target.classList.add('hide');
-    inputProjectDiv.classList.remove('hide')
-})
-
-submitProjectBtn.addEventListener('click', (e) => {
-    if(projectNameInput.value === '') return
-    addProjectBtn.classList.remove('hide');
-    inputProjectDiv.classList.add('hide')
-    newProject(projectNameInput.value)
-    renderProject()
-    renderProjectArr()
-    clearInputs()
-})
-
-
-cancelProjectBtn.addEventListener('click', () => {
-    addProjectBtn.classList.remove('hide');
-    inputProjectDiv.classList.add('hide')
-})
-
-document.querySelector('h1').addEventListener('click', () => {
-    console.log(selectedProject);
-    console.log(arr)
-})
-
-renderTasks(selectedProject)
-renderProjects()
-renderProjectArr()
-console.log(todos)
+renderTask()
+renderProject()
